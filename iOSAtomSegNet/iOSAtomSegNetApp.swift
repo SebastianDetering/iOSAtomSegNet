7e1a28e18d50c65734d5cd47c424b09e0f97d0ac
Sebastian Detering <77073983+SebastianDetering@users.noreply.github.com>
2021-12-17 12:42:36 -0800

    Initial Commit

---
+++ iOSAtomSegNet/iOSAtomSegNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAtomSegNetApp.swift
+//  iOSAtomSegNet
+//
+//  Created by sebi d on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSAtomSegNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
